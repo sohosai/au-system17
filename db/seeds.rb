028a4b852a17c83e129ba7b937f89d4b0132a907
Masato Sugiyama <sugiyama@masato.nagano.jp>
2017-09-06 09:44:17 +0900

    Update db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-ReceptionDesk.create(name: "第一エリア案内所", location:"第一エリア")
-ReceptionDesk.create(name: "第二・三エリア案内所", location:"第二・三エリア")
-ReceptionDesk.create(name: "体芸エリア案内所", location:"体芸エリア")
-ReceptionDesk.create(name: "会館エリア案内所", location:"会館エリア")
+unless ReceptionDesk.find_by name: "第一エリア案内所"
+  ReceptionDesk.create(name: "第一エリア案内所", location: "第一エリア")
+end
+unless ReceptionDesk.find_by name: "第二・三エリア案内所"
+  ReceptionDesk.create(name: "第二・三エリア案内所", location: "第二・三エリア")
+end
+unless ReceptionDesk.find_by name: "体芸エリア案内所"
+  ReceptionDesk.create(name: "体芸エリア案内所", location: "体芸エリア")
+end
+unless ReceptionDesk.find_by name: "会館エリア案内所"
+  ReceptionDesk.create(name: "会館エリア案内所", location: "会館エリア")
+end
+unless ReceptionDesk.find_by name: "実委室"
+  ReceptionDesk.create(name: "実委室", location: "実委室")
+end
