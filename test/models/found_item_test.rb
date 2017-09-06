269e572de925eb5bb238a8b94cecb13a3b5eaba8
Masato Sugiyama <sugiyama@masato.nagano.jp>
2017-09-06 16:30:33 +0900

    Update test/models/found_item_test.rb
    テストを追加する

--- test/models/found_item_test.rb
+++ test/models/found_item_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class FoundItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create(email: 'test@example.com', password: 'password', name: 'test')
+    @reception_desks = ReceptionDesk.create(name: 'test', location: 'test')
+    @found_item = FoundItem.new(resolver_id: @user.id,
+                                reception_desk_id: @reception_desks.id,
+                                receptionist_id: @user.id,
+                                name: 'test',
+                                location_found: 'test',
+                                characteristic: 'test characteristic',
+                                finder_name: 'test finder name',
+                                finder_contact: '000-000-000',
+                                note: 'test note',
+                                kind: 0,
+                                status: true)
+  end
+  test 'should be valid' do
+    assert @found_item.valid?
+  end
 end
