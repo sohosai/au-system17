3889105f0b7d8d9c8335283eb06992fd46541909
Masato Sugiyama <sugiyama@masato.nagano.jp>
2017-09-06 16:00:23 +0900

    Update found item kinds

--- app/models/found_item.rb
+++ app/models/found_item.rb
@@ -8,17 +8,18 @@ class FoundItem < ApplicationRecord
   validates :location_found, presence: true
   validates :characteristic, presence: true
   enum kind: {
-      other: 0,
-      key: 1,
-      wallet: 2,
-      mobile_phone: 3,
-      cloth_product: 4,
-      card: 5,
-      cash: 6,
-      card_case: 7,
-      keychain: 8,
-      strap: 9,
-      umbrella: 10
+    other: 0,
+    key: 1,
+    wallet: 2,
+    mobile_phone: 3,
+    electronic_device: 4,
+    clothes: 5,
+    card: 6,
+    cash: 7,
+    card_case: 8,
+    keychain: 9,
+    umbrella: 10,
+    glasses: 11
   }
 
   def set_resolved_at
