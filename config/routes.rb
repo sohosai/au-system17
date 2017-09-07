6ba5204069a1e0a4e4c9a711ec18ba591df1a334
Masato Sugiyama <sugiyama@masato.nagano.jp>
2017-09-07 17:05:45 +0900

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get "/found_items/search", to: "found_items#search"
+  post "/found_items/search", to: "found_items#search"
   resources :lost_items
   resources :found_items
   resources :reception_desks
   mount RailsAdmin::Engine => "/admin", as: "rails_admin"
   devise_for :users, path: "", path_names: { sign_in: "login", sign_out: "logout" }
   root "pages#index"
-  get "/search", to: "pages#search"
 end
