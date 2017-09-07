fc0b04474df9d304df575d8d6c9f483268db27bd
Masato Sugiyama <sugiyama@masato.nagano.jp>
2017-09-07 17:29:51 +0900

    Update app/models/found_item.rb

--- app/models/found_item.rb
+++ app/models/found_item.rb
@@ -23,6 +23,38 @@ class FoundItem < ApplicationRecord
     glasses: 11,
   }
 
+  def self.reception_desk(id)
+    if id.blank?
+      all
+    else
+      where(reception_desk_id: id)
+    end
+  end
+
+  def self.item_name(name)
+    if name.blank?
+      all
+    else
+      where(name: name)
+    end
+  end
+
+  def self.item_kind(kind)
+    if kind.blank?
+      all
+    else
+      where(kind: kind)
+    end
+  end
+
+  def self.item_characteristic(characteristic)
+    if characteristic.blank?
+      all
+    else
+      where(characteristic: characteristic)
+    end
+  end
+
   def set_resolved_at
     self.resolved_at = if status
                          Time.now.utc
