715b49df370ce5518c6817d0cc013c7138a82fbf
Masato Sugiyama <sugiyama@masato.nagano.jp>
2017-09-07 14:00:55 +0900

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => "/admin", as: "rails_admin"
   devise_for :users, path: "", path_names: { sign_in: "login", sign_out: "logout" }
   root "pages#index"
+  get "/search", to: "pages#search"
 end
