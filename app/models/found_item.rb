a834ffda5d9117b30b764f212804830593bf3deb
Masato Sugiyama <sugiyama@masato.nagano.jp>
2017-09-07 09:37:47 +0900

    Update app/models/found_item.rb

--- app/models/found_item.rb
+++ app/models/found_item.rb
@@ -7,7 +7,7 @@ class FoundItem < ApplicationRecord
   validates :kind, presence: true
   validates :location_found, presence: true
   validates :characteristic, presence: true
-  validates :status, presence: true
+  validates :status, inclusion:{in: [true, false]}
   enum kind: {
     other: 0,
     key: 1,
