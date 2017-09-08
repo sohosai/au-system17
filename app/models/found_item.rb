7947386714cb7a5a7d8e2d4acdfa802cbaaf5f7f
Masato Sugiyama <sugiyama@masato.nagano.jp>
2017-09-08 12:10:25 +0900

    Update app/models/found_item.rb

--- app/models/found_item.rb
+++ app/models/found_item.rb
@@ -3,7 +3,6 @@ class FoundItem < ApplicationRecord
   belongs_to :reception_desk
   validates :reception_desk_id, presence: true
   validates :receptionist_id, presence: true
-  validates :name, presence: true
   validates :kind, presence: true
   validates :location_found, presence: true
   validates :characteristic, presence: true
