7fc88c0c04b999e7c550a1f7509f1dba654b75b7
Masato Sugiyama <sugiyama@masato.nagano.jp>
2017-09-08 10:56:11 +0900

    Update app/models/lost_item.rb

--- app/models/lost_item.rb
+++ app/models/lost_item.rb
@@ -23,38 +23,6 @@ class LostItem < ApplicationRecord
     glasses: 11,
   }
 
-  def self.reception_desk(id)
-    if id.blank?
-      all
-    else
-      where(reception_desk_id: id)
-    end
-  end
-
-  def self.item_name(name)
-    if name.blank?
-      all
-    else
-      where("name like '%" + name + "%'")
-    end
-  end
-
-  def self.item_kind(kind)
-    if kind.blank?
-      all
-    else
-      where(kind: kind)
-    end
-  end
-
-  def self.item_characteristic(characteristic)
-    if characteristic.blank?
-      all
-    else
-      where("characteristic like '%" + characteristic + "%'")
-    end
-  end
-
   def set_resolved_at
     self.resolved_at = if status
                          Time.now.utc
