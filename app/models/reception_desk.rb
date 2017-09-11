1fca236f9062f134557fe439d8238e98ad98fb43
Masato Sugiyama <sugiyama@masato.nagano.jp>
2017-09-12 07:20:36 +0900

    Update app/models/reception_desk.rb

--- app/models/reception_desk.rb
+++ app/models/reception_desk.rb
@@ -1,6 +1,6 @@
 class ReceptionDesk < ApplicationRecord
-  has_many :found_items
-  has_many :lost_items
+  has_many :found_items, dependent: :destroy
+  has_many :lost_items, dependent: :destroy
 
   validates :name, presence: true
   validates :location, presence: true
